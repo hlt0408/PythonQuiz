5cc6bd3c6a33a566bfb2138a2f568ba1f07caa3d
hlt0408 <huanglangtao08@gmail.com>
2016-04-01 14:05:40 -0400

    Initial Commit

---
+++ Python Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Python Quiz
+//
+//  Created by Langtao Huang on 4/1/16.
+//  Copyright © 2016 hlt04. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
