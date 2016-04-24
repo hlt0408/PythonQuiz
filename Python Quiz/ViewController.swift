e7253a2809379c14b07f8a8cb9a8fb8e97004653
hlt0408 <huanglangtao08@gmail.com>
2016-04-24 10:49:12 -0400

    add json

--- Python Quiz/ViewController.swift
+++ Python Quiz/ViewController.swift
@@ -3,13 +3,13 @@
 //  Python Quiz
 //
 //  Created by Langtao Huang on 4/1/16.
+//  Modified by Degan Hao on 4/22/16.
 //  Copyright © 2016 hlt04. All rights reserved.
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var question: UILabel!
     @IBOutlet weak var input: UITextField!
@@ -24,18 +24,62 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         displayQuestion()
+        //let urlString = "file:///Users/deganhao/Desktop/quoted.json"//this is local json file which is manualluy quoted
+        let urlString = "http://adapt2.sis.pitt.edu/aggregate/GetContentLevels?usr=adl01&grp=ADL&sid=generate_a_session_id&cid=23&mod=all&models=0"// this is the file from knowledge tree
+        
+        if let url = NSURL(string: urlString) {
+            if let jsonData = try? NSData(contentsOfURL: url, options: []) {
+                
+                print(jsonData)
+                do {
+                    if let jsonResult = try NSJSONSerialization.JSONObjectWithData(jsonData, options: []) as? NSDictionary {
+                        print(jsonResult)
+                        //parseJSON(jsonResult)
+                    }
+                } catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+                
+                
+                
+                
+            }
+        }
+    }//end if let url = NSURL(string: urlString)
+    
+    
+    
+    func parseJSON(json: JSON) {
+        var labels = [String: UILabel]()
+        var strings = [String]()
+        var objects = [[String: String]]()
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            objects.append(obj)
+        }
+        
+        print(title)
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    
+    
+    
     func displayQuestion() {
-        question.text = "What is 1 + 1?"
-        correctAnswer = "2"
+        question.text = "2+1"
+        correctAnswer = "3"
         hideResponse()
     }
     
@@ -46,7 +90,7 @@ class ViewController: UIViewController {
     func showResponse() {
         response.hidden = false
     }
-
+    
     @IBAction func submitaction(sender: AnyObject) {
         let inputText = input.text;
         if inputText == correctAnswer {
@@ -58,6 +102,6 @@ class ViewController: UIViewController {
     }
     
     
-
+    
 }
 
